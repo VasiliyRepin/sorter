63ac0c02f55041b1705c128ddad04ce946befbbe
VasiliyRepin <primerior@gmail.com>
2018-02-26 15:14:28 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,31 +1,76 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        // your implementation
+        //var sorter;
+        this.arr = [];
 
-  add(element) {
-    // your implementation
-  }
+    }
 
-  at(index) {
-    // your implementation
-  }
+    add(element) {
+        // your implementation
 
-  get length() {
-    // your implementation
-  }
+        //this.element = element;
+        return this.arr.push(element);
+        //element;
 
-  toArray() {
-    // your implementation
-  }
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    at(index) {
+        // your implementation
+        return this.arr[index];
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    get length() {
+        // your implementation
+        return this.arr.length;
+    }
+
+    toArray() {
+        // your implementation
+        return this.arr
+        ;
+
+    }
+
+    sort(indices) {
+        var x;
+        var min;
+        var i;
+        var j;
+        console. log ('array initial' + this.arr);
+
+        console. log ('indices to sort' + indices);
+
+        for (i=indices.length-1; i > 0; i--) {
+            for(j= 1; j <= i; j++) {
+                if (indices[j-1] > indices[j]) {
+                    x = indices[j-1];
+                    indices[j-1] = indices[j];
+                    indices[j] = x;
+                }
+            }
+        }
+
+
+        for (i = indices.length-1; i > 0; i--) {
+            for(j= 1; j <= i; j++) {
+                if (this.arr[indices[j-1]] > this.arr[indices[j]]) {
+                    x = this.arr[indices[j-1]];
+                    this.arr[indices[j-1]] = this.arr[indices[j]];
+                    this.arr[indices[j]] = x;
+                }
+            }
+        }
+        console. log ('this array is' + this.arr);
+        return this.arr;
+
+
+
+    }
+
+    setComparator(compareFunction) {
+        // your implementation
+    }
 }
 
 module.exports = Sorter;
